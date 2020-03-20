a7411e6c4e78291a8e08daf8f3131a7d7ae99994
Zero-77 <07072u4bp6@gmail.com>
2020-03-21 00:35:50 +0800

    Initial Commit

---
+++ SwiftUI-MotoGP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-MotoGP
+//
+//  Created by 07 Zhang on 2020/3/21.
+//  Copyright © 2020 07 Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
